e046e656dc8493ecb3f8af2b4a7858f6915a1b66
Alex Martins <alex.oliveiramartins05@gmail.com>
2025-06-21 19:03:33 -0300

    Add implementation of Caesar cipher with encryption and decryption functions

---
+++ matematica-discreta/criptografia/cifra-cesar.c
@@ -0,0 +1,36 @@
+// Minha tentativa de implementacao da Cifra de Cesar
+#include <stdio.h>
+
+// ascii -> a = 65 / z = 90
+
+char encrypt(char c, int k){
+    if(k >= 26) k %= 26;
+    c += k;
+    if(c > 90) c -= 26;
+    return c;
+}
+
+char decrypt(char c, int k){
+    if(k >= 26) k % 26;
+    c -= k;
+    if(c < 65) c += 26;
+    return c;
+}
+
+int main(){
+    int k = 5;
+    
+    char nome[] = "ALEX";
+
+    for(int i = 0; i < 4; i++){
+        nome[i] = encrypt(nome[i], k);
+    }
+
+    printf("%s\n", nome);
+
+    for(int i = 0; i < 4; i++){
+        nome[i] = decrypt(nome[i], k);
+    }
+
+    printf("%s\n", nome);
+}
